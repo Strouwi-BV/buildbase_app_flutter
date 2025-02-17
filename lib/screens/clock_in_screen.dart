05c893c232e542219874c0c0da86abb49ae2ea9a
Jorik Delronge <jorik.delronge@gmail.com>
2025-02-17 16:08:07 +0100

    Registratie added

--- lib/screens/clock_in_screen.dart
+++ lib/screens/clock_in_screen.dart
@@ -11,11 +11,19 @@ class ClockInScreen extends StatefulWidget {
 }
 
 class _ClockInScreenState extends State<ClockInScreen> {
-  final StopWatchTimer _stopWatchTimer = StopWatchTimer(
-    mode: StopWatchMode.countUp,
-  );
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer(mode: StopWatchMode.countUp);
   bool isClockedIn = false;
   List<String> timeStamps = [];
+
+  // State-variabelen voor registratie
+  bool _registrationCompleted = false;
+  final _formKey = GlobalKey<FormState>();
+  String? _selectedKlantnaam;
+  String? _selectedProjectnaam;
+
+  // Opties voor de dropdownmenu's
+  final List<String> _klantnamen = ['Strouwi', 'Klant B', 'Klant C'];
+  final List<String> _projectnamen = ['Buildbase app', 'Project X', 'Project Y'];
 
   @override
   void dispose() {
@@ -47,14 +55,26 @@ class _ClockInScreenState extends State<ClockInScreen> {
   }
 
   String _formatDateTime(DateTime dateTime) {
+    // Formatteer datum en tijd zonder milliseconden
     return DateFormat('yyyy-MM-dd HH:mm:ss').format(dateTime);
   }
 
   String _formatTime(int rawTime) {
-    final int hours = rawTime ~/ (1000 * 60 * 60);
-    final int minutes = (rawTime ~/ (1000 * 60)) % 60;
-    final int seconds = (rawTime ~/ 1000) % 60;
-    return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+    final int hours = rawTime ~/ 3600000;
+    final int minutes = (rawTime % 3600000) ~/ 60000;
+    final int seconds = (rawTime % 60000) ~/ 1000;
+    return '${hours.toString().padLeft(2, '0')}:'
+           '${minutes.toString().padLeft(2, '0')}:'
+           '${seconds.toString().padLeft(2, '0')}';
+  }
+
+  void _submitRegistration() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      setState(() {
+        _registrationCompleted = true;
+      });
+    }
   }
 
   @override
@@ -74,72 +94,145 @@ class _ClockInScreenState extends State<ClockInScreen> {
           }
         },
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              StreamBuilder<int>(
-                stream: _stopWatchTimer.rawTime,
-                initialData: 0,
-                builder: (context, snapshot) {
-                  final rawTime = snapshot.data;
-                  final displayTime = _formatTime(rawTime!);
-                  return Text(
-                    displayTime,
-                    style: const TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      if (isClockedIn) {
-                        _clockOut();
-                      } else {
-                        _clockIn();
-                      }
-                    },
-                    icon: Icon(isClockedIn ? Icons.pause : Icons.play_arrow),
-                    label: Text(isClockedIn ? 'Uitklokken' : 'Inklokken'),
-                  ),
-                  const SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      _stopWatchTimer.onResetTimer();
-                      setState(() {
-                        isClockedIn = false;
-                      });
-                    },
-                    child: const Text('Reset'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: timeStamps.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(timeStamps[index]),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          _removeTimeStamp(index);
-                        },
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
+          child: _registrationCompleted ? _buildTimer() : _buildRegistrationForm(),
         ),
       ),
+    );
+  }
+
+  Widget _buildRegistrationForm() {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Text(
+              'Registratie',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                labelText: 'Klantnaam',
+                border: OutlineInputBorder(),
+              ),
+              value: _selectedKlantnaam,
+              items: _klantnamen.map((String klantnaam) {
+                return DropdownMenuItem<String>(
+                  value: klantnaam,
+                  child: Text(klantnaam),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedKlantnaam = newValue;
+                });
+              },
+              validator: (value) => value == null ? 'Selecteer een klantnaam' : null,
+              onSaved: (value) => _selectedKlantnaam = value,
+            ),
+            const SizedBox(height: 16),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                labelText: 'Projectnaam',
+                border: OutlineInputBorder(),
+              ),
+              value: _selectedProjectnaam,
+              items: _projectnamen.map((String projectnaam) {
+                return DropdownMenuItem<String>(
+                  value: projectnaam,
+                  child: Text(projectnaam),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedProjectnaam = newValue;
+                });
+              },
+              validator: (value) => value == null ? 'Selecteer een projectnaam' : null,
+              onSaved: (value) => _selectedProjectnaam = value,
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: _submitRegistration,
+              child: const Text('Start'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTimer() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '${_selectedKlantnaam!} (${_selectedProjectnaam!})',
+          style: TextStyle(fontSize: 20),
+        ),
+        const SizedBox(height: 20),
+        StreamBuilder<int>(
+          stream: _stopWatchTimer.rawTime,
+          initialData: 0,
+          builder: (context, snapshot) {
+            final rawTime = snapshot.data!;
+            final displayTime = _formatTime(rawTime);
+            return Text(
+              displayTime,
+              style: const TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          },
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              onPressed: () {
+                if (isClockedIn) {
+                  _clockOut();
+                } else {
+                  _clockIn();
+                }
+              },
+              icon: Icon(isClockedIn ? Icons.pause : Icons.play_arrow),
+              label: Text(isClockedIn ? 'Uitklokken' : 'Inklokken'),
+            ),
+            const SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: () {
+                _stopWatchTimer.onResetTimer();
+                setState(() {
+                  isClockedIn = false;
+                });
+              },
+              child: const Text('Reset'),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Expanded(
+          child: ListView.builder(
+            itemCount: timeStamps.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(timeStamps[index]),
+                trailing: IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {
+                    _removeTimeStamp(index);
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
