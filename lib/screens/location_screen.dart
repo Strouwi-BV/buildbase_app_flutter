bd7e12a5b0aa3eb1d426b29c92e1dc472aafa4e1
XavierBogers <xavier.bogers@gmail.com>
2025-02-19 13:33:45 +0100

    Added ios support for location, input fields profile
    Fixed error where IOS devices didn't receive a prompt to allow location
    Added textfields to profile_screen

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:location/location.dart';
+import 'package:location/location.dart' as loc;
+import 'package:geolocator/geolocator.dart';
 
 class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
@@ -9,12 +10,17 @@ class LocationScreen extends StatefulWidget {
   State<LocationScreen> createState() => _LocationScreenState();
 }
 
-class _LocationScreenState extends State<LocationScreen> {
-  Location location = Location();
-  bool _serviceEnabled = false;
-  PermissionStatus? _permissionGranted;
-  LocationData? _locationData;
 
+
+class _LocationScreenState extends State<LocationScreen> {
+  loc.Location location = loc.Location();
+  bool _serviceEnabled = false;
+  loc.PermissionStatus? _permissionGranted;
+  loc.LocationData? _locationData;
+  Position? _geolocatorPosition;
+
+
+//Location Package
   void _requestLocationPermission() async {
     _serviceEnabled = await location.serviceEnabled();
     if (!_serviceEnabled) {
@@ -25,9 +31,9 @@ class _LocationScreenState extends State<LocationScreen> {
     }
 
     _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
+    if (_permissionGranted == loc.PermissionStatus.denied) {
       _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
+      if (_permissionGranted != loc.PermissionStatus.granted) {
         return;
       }
     }
@@ -35,6 +41,31 @@ class _LocationScreenState extends State<LocationScreen> {
     _locationData = await location.getLocation();
 
     setState(() {});
+  }
+
+//Geolocator
+  Future<void> _getGeolocatorLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      await Geolocator.openLocationSettings();
+      return;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied){
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever){
+        return;
+      }
+    }
+
+    _geolocatorPosition = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    setState(() {
+      
+    });
   }
 
   @override
@@ -50,14 +81,30 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             ElevatedButton(
               onPressed: _requestLocationPermission,
-              child: const Text("Get Location"),
+              child: const Text("Get Location (location package)"),
+            ),
+            ElevatedButton(
+              onPressed: _getGeolocatorLocation,
+              child: const Text("Get Location (Geolocator)"),
             ),
             const SizedBox(height: 20),
-            Text("Latitude: ${_locationData?.latitude ?? "Unknown"}"),
-            Text("Longitude: ${_locationData?.longitude ?? "Unknown"}"),
+            const Text(
+              "Location Package Data:",
+              style: TextStyle(fontWeight: FontWeight.bold)
+            ),
+            Text("Latitude: ${_locationData?.latitude?.toStringAsFixed(6) ?? "Unknown"}"),
+            Text("Longitude: ${_locationData?.longitude?.toStringAsFixed(6) ?? "Unknown"}"),
+            const SizedBox(height: 20),
+            const Text(
+              "Geolocator Data:",
+              style: TextStyle(fontWeight: FontWeight.bold)
+            ),
+            Text("Geolocator Latitude: ${_geolocatorPosition?.latitude.toStringAsFixed(6) ?? "Unknown"}"),
+            Text("Geolocator Longitude: ${_geolocatorPosition?.longitude.toStringAsFixed(6) ?? "Unknown"}"),
           ],
         ),
       ),
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -15,6 +15,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Map<String, dynamic>? userProfile;
   bool isLoading = true;
 
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -26,20 +30,34 @@ class _ProfileScreenState extends State<ProfileScreen> {
     setState(() {
       userProfile = data;
       isLoading = false;
+
+      if (userProfile != null) {
+        _nameController.text = userProfile!['name'] ?? "";
+        _ageController.text = userProfile!['age']?.toString() ?? "";
+      }
     });
   }
 
-  // void updateUserProfile() async {
-  //   final success = await ApiSerive.updateUserProfile(widget.userId, {
-  //     "name": "Updated Name",
-  //     "age": userProfile?['age'] ?? 25,
-  //   });
+  void updateUserProfile() async {
+    // final success = await ApiSerive.updateUserProfile(widget.userId, {
+    //   "name": "Updated Name",
+    //   "age": userProfile?['age'] ?? 25,
+    // });
 
-    if (success) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Profile updated!")));
-      fetchUserProfile();
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Update failed!")));
+    if (_formKey.currentState!.validate()) {
+      final success = await ApiSerive.updateUserProfile(widget.userId, {
+        "name" : _nameController.text,
+        "age" : int.tryParse(_ageController.text) ?? userProfile?['age'] ?? 25,
+      });
+
+      if (success) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Profile updated!")));
+        fetchUserProfile();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Update failed!")));
+      }
     }
   }
 
@@ -63,50 +81,61 @@ class _ProfileScreenState extends State<ProfileScreen> {
           child: isLoading
               ? CircularProgressIndicator()
               : userProfile == null
-                  ? Text("Failed to load profile")
-                  : Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Name: ${userProfile!['name']}", style: TextStyle(fontSize: 20)),
-                        Text("Age: ${userProfile!['age']}", style: TextStyle(fontSize: 16)),
-                        SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: updateUserProfile,
-                          child: Text("Update Profile"),
-                        ),
-                      ],
+                  ? const Text("Failed to load profile")
+                  : Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text("Update Profile", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 10),
+                          TextFormField(
+                            controller: _nameController,
+                            decoration: const InputDecoration(
+                              labelText: "Name",
+                              border: UnderlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter a name";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 10),
+                          TextFormField(
+                            controller: _ageController,
+                            decoration: const InputDecoration(
+                              labelText: "Age",
+                              border: UnderlineInputBorder(),
+                            ),
+                            keyboardType: TextInputType.number,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Please enter a valid age";
+                              }
+                              if (int.tryParse(value) == null) {
+                                return "Please enter your age in valid numbers";
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                          Center(
+                            child: ElevatedButton(
+                              onPressed: updateUserProfile, 
+                              child: const Text("Update Profile"),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
+                  ),
         ),
       ),
     );
   }
 }
-
-// class ProfileScreen extends StatelessWidget {
-//   final Map<String, dynamic> userProfile;
-
-//   ProfileScreen({required this.userProfile});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Profile"),
-//         leading: IconButton(
-//           icon: Icon(Icons.arrow_back),
-//           onPressed: () => context.go('/'),
-//         ),
-//       ),
-//       body: GestureDetector(
-//         onHorizontalDragEnd: (details) {
-//           if (details.primaryVelocity! > 0) {
-//             context.go('/');
-//           }
-//         },
-//         child: Center(
-//           child: Text("Name: ${userProfile['name']}, Age: ${userProfile['age']}"),
-//         ),
-//       ),
-//     );
-//   }
-// }
