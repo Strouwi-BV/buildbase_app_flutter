2e294598cbc15602719cee48904d36824085ff8f
Jorik Delronge <jorik.delronge@gmail.com>
2025-02-26 11:02:30 +0100

    Added flutter map en fixed nav

--- lib/screens/event_details_screen.dart
+++ lib/screens/event_details_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
 
-class EventDetailsScreen extends StatelessWidget {
+class EventDetailsScreen extends StatefulWidget {
   final String formattedDate;
   final String clockInTime;
   final String clockOutTime;
@@ -16,14 +18,41 @@ class EventDetailsScreen extends StatelessWidget {
     required this.clockOutTime,
     required this.noteText,
     required this.onEditPressed,
-    required this.location
+    required this.location,
   }) : super(key: key);
+
+  @override
+  _EventDetailsScreenState createState() => _EventDetailsScreenState();
+}
+
+class _EventDetailsScreenState extends State<EventDetailsScreen> {
+  latLng.LatLng? _locationCoordinates;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCoordinates();
+  }
+
+  void _fetchCoordinates() async {
+    try {
+      List<Location> locations = await locationFromAddress(widget.location);
+      if (locations.isNotEmpty) {
+        setState(() {
+          _locationCoordinates = latLng.LatLng(
+              locations[0].latitude, locations[0].longitude);
+        });
+      }
+    } catch (e) {
+      print('Error fetching coordinates: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(formattedDate),
+        title: Text(widget.formattedDate),
         backgroundColor: Color(0xff13263B),
         foregroundColor: Colors.white,
         leading: IconButton(
@@ -36,13 +65,44 @@ class EventDetailsScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Ingeklokt: $clockInTime'),
+            Text('Ingeklokt: ${widget.clockInTime}'),
             SizedBox(height: 8),
-            Text('Uitgeklokt: $clockOutTime'),
+            Text('Uitgeklokt: ${widget.clockOutTime}'),
             SizedBox(height: 8),
-            Text('Notities: $noteText'),
+            Text('Notities: ${widget.noteText}'),
             SizedBox(height: 8),
-            Text('Locatie: $location'),
+            Text('Locatie: ${widget.location}'),
+            SizedBox(height: 16),
+            if (_locationCoordinates != null)
+              Expanded(
+                child: FlutterMap(
+                  options: MapOptions(
+                    center: _locationCoordinates,
+                    zoom: 13.0,
+                  ),
+                  children: [
+                    TileLayer(
+                      urlTemplate:
+                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      userAgentPackageName: 'com.example.app',
+                    ),
+                    MarkerLayer(
+                      markers: [
+                        Marker(
+                          width: 80.0,
+                          height: 80.0,
+                          point: _locationCoordinates!,
+                          child: Icon(
+                            Icons.location_on,
+                            color: Colors.red,
+                            size: 40.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             SizedBox(height: 16),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
@@ -51,7 +111,7 @@ class EventDetailsScreen extends StatelessWidget {
               ),
               child: Text('Bewerk'),
               onPressed: () {
-                onEditPressed();
+                widget.onEditPressed();
               },
             ),
           ],
