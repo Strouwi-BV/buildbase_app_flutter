68df144fe08e73c189c7e0835b12204187776d8d
FelixDePrins <felix.deprins@student.ucll.be>
2025-03-06 14:15:44 +0100

    It works!

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -1,6 +1,7 @@
 import 'package:buildbase_app_flutter/service/api_service.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:email_validator/email_validator.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({Key? key}) : super(key: key);
@@ -11,15 +12,14 @@ class ForgotPasswordScreen extends StatefulWidget {
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   final TextEditingController _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   bool startVisible = true;
 
   Future<void> forgotPassword(BuildContext context) async {
+    if (!_formKey.currentState!.validate()) return;
+
     final String email = _emailController.text.trim();
-    if (email.isEmpty) {
-      showErrorDialog(context, "Please enter an email address.");
-      return;
-    }
 
     final apiService = ApiService();
     final loginResponse = await apiService.forgotPassword(email);
@@ -80,48 +80,60 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
               ),
               Visibility(
                 visible: startVisible,
-                child: Container(
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5),
-                    boxShadow: const [
-                      BoxShadow(color: Colors.grey, blurRadius: 1),
-                    ],
-                  ),
-                  child: Column(
-                    children: [
-                      TextField(
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () => forgotPassword(context),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
+                child: Form(
+                  key: _formKey,
+                  child: Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: const [
+                        BoxShadow(color: Colors.grey, blurRadius: 1),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(),
                           ),
-                          minimumSize: const Size.fromHeight(50),
+                          autofillHints: [AutofillHints.email],
+                          validator: (email) {
+                            if (email == null || email.isEmpty) {
+                              return 'Please enter your email';
+                            } else if (!EmailValidator.validate(email)) {
+                              return 'Enter a valid email';
+                            }
+                            return null;
+                          },
                         ),
-                        child: const Text(
-                          'Renew',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () => forgotPassword(context),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            minimumSize: const Size.fromHeight(50),
+                          ),
+                          child: const Text(
+                            'Renew',
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                    ],
+                        const SizedBox(height: 20),
+                      ],
+                    ),
                   ),
                 ),
               ),
               Visibility(
                 visible: !startVisible,
-                child: Text(
+                child: const Text(
                   "An email has been sent to your email address with instructions to reset your password.",
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   textAlign: TextAlign.center,
