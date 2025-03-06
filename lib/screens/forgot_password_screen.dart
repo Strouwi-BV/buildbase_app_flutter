6c98c63a79f20fbb17558b6b9e589dccd722b40c
FelixDePrins <felix.deprins@student.ucll.be>
2025-03-06 13:35:01 +0100

    Email works only validation left

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -1,13 +1,65 @@
-import 'package:buildbase_app_flutter/screens/header_bar_screen.dart';
+import 'package:buildbase_app_flutter/service/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class ForgotPasswordScreen extends StatelessWidget {
-  const ForgotPasswordScreen({super.key});
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final TextEditingController _emailController = TextEditingController();
+
+  bool startVisible = true;
+
+  Future<void> forgotPassword(BuildContext context) async {
+    final String email = _emailController.text.trim();
+    if (email.isEmpty) {
+      showErrorDialog(context, "Please enter an email address.");
+      return;
+    }
+
+    final apiService = ApiService();
+    final loginResponse = await apiService.forgotPassword(email);
+
+    if (loginResponse != null) {
+      setState(() {
+        startVisible = false;
+      });
+    } else {
+      showErrorDialog(context, "Invalid email, please try again.");
+    }
+  }
+
+  void showErrorDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            title: const Text('Error'),
+            content: Text(message),
+            actions: [
+              TextButton(
+                onPressed: () => context.pop(),
+                child: const Text('Ok'),
+              ),
+            ],
+          ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const HeaderBar(title: 'Forgot Password'),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff13263B),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => context.pop(),
+        ),
+      ),
       backgroundColor: Colors.white,
       body: Center(
         child: Padding(
@@ -18,43 +70,61 @@ class ForgotPasswordScreen extends StatelessWidget {
               Image.asset('assets/Logo_zwart.png', height: 100),
               Padding(
                 padding: const EdgeInsets.only(bottom: 5),
-                child: Text(
-                  "Forgot your password?",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                child: Visibility(
+                  visible: startVisible,
+                  child: const Text(
+                    "Forgot your password?",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                  boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 1)],
-                ),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
+              Visibility(
+                visible: startVisible,
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                    boxShadow: const [
+                      BoxShadow(color: Colors.grey, blurRadius: 1),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
                         ),
-                        minimumSize: const Size.fromHeight(50),
                       ),
-                      child: const Text(
-                        'renew',
-                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () => forgotPassword(context),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          minimumSize: const Size.fromHeight(50),
+                        ),
+                        child: const Text(
+                          'Renew',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                  ],
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: !startVisible,
+                child: Text(
+                  "An email has been sent to your email address with instructions to reset your password.",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  textAlign: TextAlign.center,
                 ),
               ),
             ],
